61508498f7fd6162aa491dfcb18fd026aabfe72d
caps.liu@foxmail.com <caps.liu@foxmail.com>
2015-10-20 19:42:13 +0800

    Add local package into openwrt Signed-off-by: caps.liu@foxmail.com <caps.liu@foxmail.com>

---
+++ package/ace/files/ACE_wrappers/ace/config.h
@@ -0,0 +1,11 @@
+/*
+*  Building ace on Linux plateform, so include in config-linux.h
+*/
+
+#define ACE_HAS_IPV6
+#undef ACE_NLOGGING
+#define ACE_HAS_POSITION_INDEPENDENT_POINTERS 0
+
+#include "ace/config-linux.h"
+
+
