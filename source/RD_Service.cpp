71ca31d33012db11ddbfb9c9091722aa25d01d4c
caps.liu@foxmail.com <caps.liu@foxmail.com>
2015-10-27 23:46:50 +0800

    refeactor code Signed-off-by: caps.liu@foxmail.com <caps.liu@foxmail.com>

--- source/RD_Service.cpp
+++ source/RD_Service.cpp
@@ -5,13 +5,16 @@
 #include "RD_Service.h"
 #include "CoAP_Wrapper.h"
 #include "CoAP_RD_Resource.h"
+#include "CfgService.h"
+#include "NET_Service.h"
 
 
-
-RDService::RDService(CoAPWrapper* ctx)
-:rd_resource_(new CoAP_RD_Resource(ctx))
+RDService::RDService(CfgService *conf, NetService *net)
+:conf_(conf),
+net_(net),
+coap_wrapper_(0),
+rd_resource_(0)
 {
-    ACE_ASSERT(rd_resource_ != 0);
 }
 
 RDService::~RDService()
@@ -19,20 +22,90 @@ RDService::~RDService()
 }
 
 int RDService::Init()
-{  
-    if (rd_resource_)
+{
+    ACE_Time_Value timeout;
+    timeout.sec(5);
+    
+    if ((coap_wrapper_ = new CoAPWrapper()) == 0)
     {
-        return rd_resource_->Create();
+        ACE_DEBUG((LM_DEBUG, "Failed to allocate CoAPWrapper in RD\n"));
+        return -1;
     }
+
+    if (coap_wrapper_->Create(conf_->rd_addr_, 
+                              conf_->rd_addr_port_,
+                              conf_->coap_debug_level_
+                              ) < 0)
+    {
+
+        ACE_DEBUG((LM_DEBUG,"Failed to create coap\n"));
+        return -1;
+    }
+
+    if ((rd_resource_ = new CoAP_RD_Resource(coap_wrapper_)) == 0)
+    {
+        ACE_DEBUG((LM_DEBUG, "Failed to allocate rd resource in RD\n"));
+        return -1;
+    }
+
+    if ((rd_resource_->Create()) < 0)
+    {
+        ACE_DEBUG((LM_DEBUG,"Failed to craete rd resource\n"));
+        return -1;
+    }
+
+    net_->join(coap_wrapper_->get_handle());
+    net_->RegHandler(this, ACE_Event_Handler::READ_MASK);
+    net_->schedule_timer(this, 0, timeout);
     
     return 0;
 }
 
 int RDService::Close()
 {
+    if (coap_wrapper_)
+    {
+        delete coap_wrapper_;
+    }
+
     if (rd_resource_ )
+    {
         delete rd_resource_;
+    }
         
     return 0;
+}
+
+int RDService::handle_input (ACE_HANDLE fd)
+{
+    if (coap_wrapper_)
+        coap_wrapper_->handle_event();
+        
+    return 0;
+}
+
+int RDService::handle_timeout (const ACE_Time_Value &tv,
+                            const void *arg)
+{
+    ACE_Time_Value timeout;
+    timeout.sec(5);
+    
+    coap_wrapper_->time_out(timeout);
+    net_->schedule_timer(this, 0, timeout);
+    
+    return 0;
+}
+
+ACE_HANDLE RDService::get_handle (void) const
+{
+    ACE_DEBUG((LM_DEBUG,
+               "call RDService get_handle\n"));
+               
+    if (coap_wrapper_)
+    {
+        return coap_wrapper_->get_handle();
+    }
+
+    return -1;    
 }
 
--- source/RD_Service.h
+++ source/RD_Service.h
@@ -9,19 +9,31 @@
 
 class CoAPWrapper;
 class CoAP_RD_Resource;
+class CfgService;
+class NetService;
 
-class RDService
+class RDService : public ACE_Event_Handler
 {
 public:
 
-    RDService(CoAPWrapper* ctx);
+    RDService(CfgService *conf, NetService *net);
     ~RDService();
 
     int Init();
     int Close();
 
+    virtual int handle_input (ACE_HANDLE fd);
+    virtual int handle_timeout (const ACE_Time_Value &tv,
+                                const void *arg);
+    
+    virtual ACE_HANDLE get_handle (void) const;
+
 private:
+    
     CoAP_RD_Resource* rd_resource_;
+    CfgService *conf_;
+    NetService *net_;
+    CoAPWrapper *coap_wrapper_;
     
 };
 
